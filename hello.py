a1d82f121e77034d9274325ff601902385fd57c0
Manoj Pandey <manojpandey1996@gmail.com>
2015-07-10 23:02:04 +0530

    added user sessions

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -6,10 +6,8 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -33,12 +31,11 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 if __name__ == '__main__':
